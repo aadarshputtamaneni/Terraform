3703307995716bb77a83e65340b4812382004778
aadarsh <aadarsh153@gmail.com>
2025-05-22 20:13:11 +0530

    Terraform

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -1,9 +1,9 @@
 # resource <resource-type> <resource-name>
 resource "aws_instance" "db" {
 
-    ami = "ami-090252cbe067a9e58"
+    ami = "ami-09c813fb71547fc4f"
     vpc_security_group_ids = [aws_security_group.allow_ssh.id]
-    instance_type = "t3.micro"
+    instance_type = "t2.micro"
 
     tags = {
         Name = "db"
@@ -31,7 +31,7 @@ resource "aws_security_group" "allow_ssh" {
 
     tags = {
         Name = "allow_ssh"
-        CreatedBy = "Sivakumar"
+        CreatedBy = "aadarsh"
     }
 }
 
