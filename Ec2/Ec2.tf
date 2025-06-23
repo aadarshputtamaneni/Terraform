e6dd054a8d378ea10c7c0d94fcd1975faa82e4e1
aadarsh <aadarsh153@gmail.com>
2025-06-24 02:18:50 +0530

    update

--- Ec2/Ec2.tf
+++ Ec2/Ec2.tf
@@ -16,12 +16,12 @@ resource "aws_security_group" "allow_ssh" {
 
     #terraform block
     ingress {
-        from_port        = 22
-        to_port          = 22
+        from_port        = 0
+        to_port          = 0
         protocol         = "tcp"
         cidr_blocks      = ["0.0.0.0/0"]
-    }
 
+    }
     egress {
         from_port        = 0 # from 0 to 0 means, opening all protocols
         to_port          = 0
